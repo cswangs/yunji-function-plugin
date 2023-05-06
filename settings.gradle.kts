215a3698e2bb342fe98e01bbc5a54706709d8382
GitHub Action <action@github.com>
2023-05-06 08:04:19 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "yunji-function-plugin"
--- src/main/kotlin/com/github/icemaples/yunjifunctionplugin/MyBundle.kt
+++ src/main/kotlin/com/github/icemaples/yunjifunctionplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.icemaples.yunjifunctionplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
